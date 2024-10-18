a0023d47025081f1c15674b20e61d98fba8b52e7
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-18 16:32:49 -0600

    Unicafe, paso 2

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,35 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allComments, setAllComments] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   const addGoodComment = () => {
     console.log("Good value:", good);
+    setAllComments(allComments + 1);
     setGood(good + 1);
+
+    setPositive((good * 100) / allComments);
   };
 
   const addNeutralComment = () => {
     console.log("Neutral value:", neutral);
+    setAllComments(allComments + 1);
     setNeutral(neutral + 1);
+
+    setPositive((good * 100) / allComments);
   };
 
   const addBadComment = () => {
     console.log("Bad value:", bad);
+    setAllComments(allComments + 1);
     setBad(bad + 1);
+
+    setPositive((good * 100) / allComments);
   };
+
+  const calculateAverage = good * 1 + neutral * 0 + bad * -1;
+  const finalAverage = calculateAverage / allComments;
 
   return (
     <div>
@@ -43,6 +57,9 @@ const App = () => {
       <Display txt="good" value={good} />
       <Display txt="neutral" value={neutral} />
       <Display txt="bad" value={bad} />
+      <Display txt="all" value={allComments} />
+      <Display txt="average" value={finalAverage} />
+      <Display txt="positive" value={positive} />
     </div>
   );
 };
